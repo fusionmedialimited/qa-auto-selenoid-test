98db0a3bce624d923da2c4f1d046005021c8ab31
kami46nikita <kami46nikita@gmail.com>
2024-04-12 11:03:45 +0300

    retry listener dropped

--- src/test/java/testRuns/CoreRegressionTestRun.java
+++ src/test/java/testRuns/CoreRegressionTestRun.java
@@ -1,10 +1,8 @@
 package testRuns;
 
-import infrastructure.listeners.retry.common.CucumberAnnotationTransformer;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Listeners;
 
 @CucumberOptions(
         features = {
